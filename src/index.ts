16fbd840ebaee4c0d76730b68a6a5abb5ba7452d
amuliawijaya23 <amuliawijaya@gmail.com>
2024-05-09 23:14:23 +0700

    adds prettier config

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,20 @@
 import express, { Express, Request, Response } from 'express';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+import cors from 'cors';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const PORT = process.env.PORT;
 
-const app = express();
+const App = express();
 
-app.listen(PORT, () => {
+App.use(bodyParser.urlencoded({ extended: true }));
+App.use(bodyParser.json());
+App.use(cors());
+App.use(morgan('dev'));
+
+App.listen(PORT, () => {
   console.log(`Running on port ${PORT}`);
 });
