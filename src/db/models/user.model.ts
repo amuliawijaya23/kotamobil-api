1194107f0512ff75ed1b27d164ac37e21d0d4b98
amuliawijaya23 <amuliawijaya@gmail.com>
2024-05-18 18:56:35 +0700

    updates user model

--- src/db/models/user.model.ts
+++ src/db/models/user.model.ts
@@ -6,7 +6,6 @@ const userSchema = new Schema({
   email: { type: String, required: true },
   authentication: {
     password: { type: String, required: true, select: false },
-    salt: { type: String, select: false },
     sessionToken: { type: String, select: false },
   },
 });
