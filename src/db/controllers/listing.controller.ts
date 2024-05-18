7716ea2c6d8b556b2980c1417f6f6611c8a9b547
amuliawijaya23 <amuliawijaya@gmail.com>
2024-05-19 00:16:28 +0700

    updates import with path config

--- src/db/controllers/listing.controller.ts
+++ src/db/controllers/listing.controller.ts
@@ -7,7 +7,7 @@ import {
   createListing,
   deleteListingById,
   updateListingById,
-} from '../actions/listing.action';
+} from '@db/actions/listing.action';
 
 export const allListings = async (request: Request, response: Response) => {
   try {
@@ -88,21 +88,21 @@ export const updateListing = async (request: Request, response: Response) => {
     const formData = request.body;
 
     if (
-      ('name' in formData && formData['name']) ||
-      ('vin' in formData && formData['vin']) ||
-      ('make' in formData && formData['make']) ||
-      ('model' in formData && formData['model']) ||
-      ('year' in formData && formData['year']) ||
-      ('odometer' in formData && formData['odometer']) ||
-      ('color' in formData && formData['color']) ||
-      ('condition' in formData && formData['condition']) ||
-      ('plateNumber' in formData && formData['plateNumber']) ||
-      ('assembly' in formData && formData['assembly']) ||
-      ('transmission' in formData && formData['transmission']) ||
-      ('fuelType' in formData && formData['fuelType']) ||
-      ('taxDate' in formData && formData['taxDate']) ||
-      ('price' in formData && formData['price']) ||
-      ('dateAdded' in formData && formData['dateAdded'])
+      ('name' in formData && !formData.name) ||
+      ('vin' in formData && !formData.vin) ||
+      ('make' in formData && !formData.make) ||
+      ('model' in formData && !formData.model) ||
+      ('year' in formData && !formData.year) ||
+      ('odometer' in formData && !formData.odometer) ||
+      ('color' in formData && !formData.color) ||
+      ('condition' in formData && !formData.condition) ||
+      ('plateNumber' in formData && !formData.plateNumber) ||
+      ('assembly' in formData && !formData.assembly) ||
+      ('transmission' in formData && !formData.transmission) ||
+      ('fuelType' in formData && !formData.fuelType) ||
+      ('taxDate' in formData && !formData.taxDate) ||
+      ('price' in formData && !formData.price) ||
+      ('dateAdded' in formData && !formData.dateAdded)
     ) {
       response.sendStatus(400);
     }
