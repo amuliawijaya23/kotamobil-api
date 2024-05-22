b6e85f3974de2ef3078d9c70dabff5e2b0bdcaf1
amuliawijaya23 <amuliawijaya@gmail.com>
2024-05-23 00:17:49 +0700

    updates user model to set email to unique instead of first name

--- src/db/models/user.model.ts
+++ src/db/models/user.model.ts
@@ -1,9 +1,9 @@
 import mongoose, { Schema } from 'mongoose';
 
 const userSchema = new Schema({
-  firstName: { type: String, required: true, unique: true },
+  firstName: { type: String, required: true },
   lastName: String,
-  email: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   picture: String,
 });
