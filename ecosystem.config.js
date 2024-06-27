638f0df9ec8ec4735e1bdfc43a381b36c5a2d526
amuliawijaya23 <amuliawijaya@gmail.com>
2024-06-27 15:44:20 +0700

    adds pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  apps: [
+    {
+      name: 'kotamobil-api',
+      script: './dist/index.js',
+    },
+  ],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-54-255-187-207.ap-southeast-1.compute.amazonaws.com',
+      key: '~/.ssh/kota-mobil-api-key.pem',
+      ref: 'origin/main',
+      repo: 'git@github.com:amuliawijaya23/kotamobil-api.git',
+      path: '/home/ubuntu/kotamobil-api',
+      'post-deploy':
+        'npm install && npm run build &&  pm2 startOrRestart ecosystem.config.js',
+    },
+  },
+};
